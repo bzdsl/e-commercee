135ac812039a33c15123c221c2a0f56a28c58bfc
bzdsl <1851060093@e.tlu.edu.vn>
2026-01-02 11:28:48 +0700

    Seperate into smaller components

--- frontend/src/pages/Orders/Orders.jsx
+++ frontend/src/pages/Orders/Orders.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { formatMoney } from "../../utils/money";
 import dayjs from "dayjs";
+import OrderDetails from "./OrderDetails";
 const Orders = ({ cart }) => {
   const [orders, setOrders] = useState([]);
 
@@ -42,49 +43,7 @@ const Orders = ({ cart }) => {
                   </div>
                 </div>
 
-                <div className="order-details-grid">
-                  {order.products.map((orderProduct) => {
-                    return (
-                      <Fragment key={orderProduct.product.id}>
-                        <div className="product-image-container">
-                          <img src={orderProduct.product.image} />
-                        </div>
-
-                        <div className="product-details">
-                          <div className="product-name">
-                            {orderProduct.product.name}
-                          </div>
-                          <div className="product-delivery-date">
-                            Arriving on:{" "}
-                            {dayjs(orderProduct.estimatedDeliveryTimeMs).format(
-                              "MMMM D"
-                            )}
-                          </div>
-                          <div className="product-quantity">
-                            Quantity: {orderProduct.quantity}
-                          </div>
-                          <button className="buy-again-button button-primary">
-                            <img
-                              className="buy-again-icon"
-                              src="images/icons/buy-again.png"
-                            />
-                            <span className="buy-again-message">
-                              Add to Cart
-                            </span>
-                          </button>
-                        </div>
-
-                        <div className="product-actions">
-                          <a href="/tracking">
-                            <button className="track-package-button button-secondary">
-                              Track package
-                            </button>
-                          </a>
-                        </div>
-                      </Fragment>
-                    );
-                  })}
-                </div>
+                <OrderDetails order={order} />
               </div>
             );
           })}
